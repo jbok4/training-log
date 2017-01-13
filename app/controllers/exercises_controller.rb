fb8be53ab3263e630f601c90852ed0f7d8f7b3cb
jbok4 <jbok4@aol.com>
2017-01-13 17:23:05 -0500

    training log css set

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,15 +1,26 @@
 class ExercisesController < ApplicationController
 
   def index
-    @exercises = Exercise.all
-    render json: @exercises
+    @workout = Workout.find(params[:workout_id])
+    @exercises = @workout.exercises
   end
+
+  def new
+    @workout = Workout.find(params[:workout_id])
+    @exercise = @workout.exercises.build
 
   def create
     @workout = Workout.find(params[:workout_id])
-    @exercise = @workout.exercises.create(params[:workout].permit(:name, :sets, :reps))
+    @exercise = @workout.exercises.create(params[:exercise])
 
     redirect_to workout_path(@workout)
+  end
+
+
+
+  def show
+    @workout = Workout.find(params[:workout_id])
+    @exercise = @workout.exercises.find(params[:id])
   end
 
 
